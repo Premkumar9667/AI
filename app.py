b2923f7c26a8d90f387d3af460841b17e2861fc4
Premkumar9667 <145314894+Premkumar9667@users.noreply.github.com>
2025-09-10 10:30:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -241,4 +241,7 @@ def chat():
 
 if __name__ == '__main__':
     app.logger.info("Startup complete. If chatbot retrieval fails, run: python build_index.py")
-    app.run(host='127.0.0.1', port=5000, debug=True)
+
+   
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
